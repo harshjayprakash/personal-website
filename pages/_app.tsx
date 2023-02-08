d8d94f8ca27676da71916371101dbad33228d440
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-08 12:47:38 +0000

    Imported footer.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "./../style/base.css";
 import "./../style/colours.css";
 
 import Header from "./../components/header";
+import Footer from "./../components/footer";
 
 export default function App(): JSX.Element {
     return (
