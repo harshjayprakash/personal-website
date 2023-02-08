98753f68ec1ae8a1240a2c972daf1c7ca13bdde1
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-08 13:56:43 +0000

    Header links to pages added.

--- components/header.tsx
+++ components/header.tsx
@@ -4,9 +4,9 @@ export default function Header(): JSX.Element {
             <span className="header__logo">Harsh.</span>
             <input type="checkbox" id="header__nav-toggle" className="header__nav-toggle"/>
             <nav className="header__nav">
-                <a className="header__nav-link">Home</a>
-                <a className="header__nav-link">Projects</a>
-                <a className="header__nav-link">About</a>
+                <a className="header__nav-link" href="/">Home</a>
+                <a className="header__nav-link" href="/projects">Projects</a>
+                <a className="header__nav-link" href="/about">About</a>
             </nav>
             <label htmlFor="header__nav-toggle" className="header__nav-label">
                 <span className="header__nav-label__design"></span>
