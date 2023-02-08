00825dd7679be320bb1ee834d6a75d1bd0e9b003
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-08 13:55:28 +0000

    Added routing ability.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,14 @@ import "./../style/header.css";
 import Header from "./../components/header";
 import Footer from "./../components/footer";
 
-export default function App(): JSX.Element {
+export default function App({Component, pageProps}): JSX.Element {
     return (
         <>
         <Header />
-        <h1>Application</h1>
+        <br/><br/><br/>
+        <main>
+        <Component {...pageProps} />
+        </main>
         <Footer />
         </>
     );
