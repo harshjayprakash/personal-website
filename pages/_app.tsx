223b05ff93049d372f17f0ae2af4970b7e8051f5
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-08 12:04:02 +0000

    Added header to main app file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
+import Header from './../components/header';
+
 export default function App(): JSX.Element {
     return (
+        <>
+        <Header />
         <h1>Application</h1>
+        </>
     );
 }
