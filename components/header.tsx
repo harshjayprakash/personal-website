d1a7f6f1ebda895fd8d9b392f83aedf1b9dc1f62
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-08 12:17:07 +0000

    Added header content.

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,16 @@
 export default function Header(): JSX.Element {
     return (
-        <header>
-            Header
+        <header className="header">
+            <span className="header__logo">Harsh.</span>
+            <input type="checkbox" id="header__nav-toggle" className="header__nav-toggle"/>
+            <nav className="header__nav">
+                <a className="header__nav-link">Home</a>
+                <a className="header__nav-link">Projects</a>
+                <a className="header__nav-link">About</a>
+                <label htmlFor="header__nav-toggle" className="header__nav-label">
+                    <span className="header__nav-label__design">X</span>
+                </label>
+            </nav>
         </header>
-    )
+    );
 }
