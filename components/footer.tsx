7af0c214cc40a9b4b4699f4e55201cb4ee9d8d44
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-08 12:47:02 +0000

    Created footer component.

---
+++ components/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(): JSX.Element {
+    return (
+        <footer className="footer">
+            <p className="footer__date">2023. Harsh Jayprakash.</p>
+            <p className="footer__msg">Under Construction.</p>
+        </footer>
+    );
+}
