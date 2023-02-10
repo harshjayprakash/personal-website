5626279f5ae0a5c55960f7e08adc64ec89b9248a
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-10 09:54:14 +0000

    Imported spacing stylesheet.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "./../style/base.css";
 import "./../assets/fonts/fonts.css";
 import "./../style/colours.css";
 import "./../style/header.css";
+import "./../style/spacing.css";
 
 import Header from "./../components/header";
 import Footer from "./../components/footer";
