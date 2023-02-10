45f74cd6e75fa95534dfb045eedfd35a26ce5c2d
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-10 11:00:03 +0000

    Create projects information file.

---
+++ data/projects.tsx
@@ -0,0 +1,26 @@
+export const programmingProjects = [
+    {
+        name: "Personal Website",
+        languages: "Typescript • CSS",
+        tools: "Visual Studio Code",
+        frameworks: "Next.JS",
+        platform: "Web-based • Any that supports a modern web browser",
+        description: "A personal website to showcase any projects created using the Next.JS framework.",
+    },
+    {
+        name: "The Blok Experiment",
+        languages: "C",
+        tools: "Visual Studio Code • GCC",
+        frameworks: "WIN32 API",
+        platform: "Desktop • Windows NT Only",
+        description: "An experimeent to learn how native Windows applications are created with the WIN32 API.",
+    },
+    {
+        name: "Wrapping Paper Quotes Calculator",
+        languages: "Python",
+        tools: "Visual Studio Code",
+        frameworks: "Tkinter",
+        platform: "Desktop • Any that supports the Python Interpreter.",
+        description: "A program to demonstrate a 'proof of concept' for a department, who provide gift wrapping services based on the shape of the present.",
+    }
+];
