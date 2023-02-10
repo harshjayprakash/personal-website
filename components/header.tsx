1d1daf7c9b8f7843a4461cd1b7e560bd069a9d58
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-10 10:45:06 +0000

    Dynamically adds links based on paths file.

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,22 @@
+import { useRouter } from "next/router";
+import { paths } from "./../data/paths";
+
 export default function Header(): JSX.Element {
+    const router = useRouter();
+
+    var navLinks: Array<JSX.Element> = new Array<JSX.Element>();
+    for (let i = 0; i < paths.length; i++) {
+        navLinks.push(
+            <a className="header__nav-link" href={paths[i].path}>{paths[i].name}</a>
+        );
+    }
+
     return (
         <header className="header">
             <span className="header__logo">Harsh.</span>
             <input type="checkbox" id="header__nav-toggle" className="header__nav-toggle"/>
             <nav className="header__nav">
-                <a className="header__nav-link" href="/">Home</a>
-                <a className="header__nav-link" href="/projects">Projects</a>
-                <a className="header__nav-link" href="/about">About</a>
+                <>{navLinks}</>
             </nav>
             <label htmlFor="header__nav-toggle" className="header__nav-label">
                 <span className="header__nav-label__design"></span>
