c0f03b36ceacbd834c5ecc12bfc5bae4c828837f
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-10 11:14:21 +0000

    Added details to project page.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,12 @@
 export default function Projects(): JSX.Element {
     return (
+        <>
         <h1>Projects</h1>
+        <p>Below shows the projects I have undertaken both during education and within my own time.</p>
+        <br/><br/>
+        <section className="project-cards">
+            <p>Project Cards Go Here.</p>
+        </section>
+        </>
     );
 }
