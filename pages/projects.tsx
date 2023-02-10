8ac2fab899b0ca9a60a84450200c6df4e8799122
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-10 11:18:57 +0000

    Added cards to page.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,20 @@
+import Card from "../components/card";
+import { programmingProjects } from "../data/projects";
+
 export default function Projects(): JSX.Element {
+    
+    var projects: Array<JSX.Element> = new Array<JSX.Element>();
+    for (let i = 0; i < programmingProjects.length; i++) {
+        projects.push(<Card {...programmingProjects[i]} />)
+    }
+
     return (
         <>
         <h1>Projects</h1>
         <p>Below shows the projects I have undertaken both during education and within my own time.</p>
         <br/><br/>
         <section className="project-cards">
-            <p>Project Cards Go Here.</p>
+            <>{projects}</>
         </section>
         </>
     );
