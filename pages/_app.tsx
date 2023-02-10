eda79069ee026c4b7fc3fd91d8a7dbe9d1d1d887
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-10 09:53:00 +0000

    Added class name 'content' to main tag.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({Component, pageProps}): JSX.Element {
         <>
         <Header />
         <br/><br/><br/>
-        <main>
+        <main className="content">
         <Component {...pageProps} />
         </main>
         <Footer />
