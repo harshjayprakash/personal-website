5461b32950dcf4de5443aa5c552b5453f1233bc4
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-11 15:31:39 +0000

    Updated arrow style.

--- components/project-header.tsx
+++ components/project-header.tsx
@@ -1,7 +1,7 @@
 export default function ProjectHeader({ name, languages, tools, frameworks, platform }): JSX.Element {
     return (
         <section className="project-header">
-            <a className="link" href="/projects">&lt; Projects</a><br/>
+            <a className="link" href="/projects">&larr; Projects</a><br/>
             <h2>{name}</h2>
             <small>
                 Languages: {languages} <br/>
