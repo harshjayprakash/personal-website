e8480747e45601c8a424f7a902b365c5b7c4a7fd
Harsh Jayprakash <harshjayprakash@outlook.com>
2023-02-25 16:00:10 +0000

    Created skills data.

---
+++ data/skills.tsx
@@ -0,0 +1,10 @@
+export const technicalSkills = [
+    {
+        name: "Web Development",
+        languages: "HTML • CSS • Next.JS"
+    },
+    {
+        name: "Desktop Development",
+        languages: "Python • Visual Basic .NET • C • Java*"
+    }
+]
